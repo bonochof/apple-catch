98cc4b352def4e0f0f4b403ef3ef3b0915b51e7d
bonochof <ryos427@yahoo.co.jp>
2018-10-14 15:28:44 +0900

    add scene

--- main.rb
+++ main.rb
@@ -10,7 +10,8 @@ Sound.register(:get, 'sounds/get.wav')
 Sound.register(:explosion, 'sounds/explosion.wav')
 
 GAME_INFO = {
-  score: 0
+  scene: :title,
+  score: 0,
 }
 
 class Player < Sprite
@@ -67,7 +68,7 @@ class Bomb < Item
   def hit
     Sound[:explosion].play
     self.vanish
-    GAME_INFO[:score] = 0
+    GAME_INFO[:scene] = :game_over
   end
 end
 
@@ -105,13 +106,29 @@ Window.load_resources do
   items = Items.new
   
   Window.loop do
-    player.update
-    items.update(player)
-    
-    Window.draw_box_fill(0, 0,Window.width, GROUND_Y, [128, 255, 255])
+    Window.draw_box_fill(0, 0, Window.width, GROUND_Y, [128, 255, 255])
     Window.draw_box_fill(0, GROUND_Y, Window.width, Window.height, [0, 128, 0])
-    Window.draw_font(0, 0, "SCORE: #{GAME_INFO[:score]}", Font.default)
-    player.draw
-    items.draw
+    Window.draw_font(0, 0, "SCORE: #{GAME_INFO[:score]}", Font.default, :color=>[0, 0, 0])
+    
+    case GAME_INFO[:scene]
+    when :title
+      Window.draw_font(240, 120, "PRESS SPACE", Font.default, :color=>[200, 0, 0])
+      GAME_INFO[:scene] = :playing if Input.key_push?(K_SPACE)
+    when :playing
+      player.update
+      items.update(player)
+      player.draw
+      items.draw
+    when :game_over
+      player.draw
+      items.draw
+      Window.draw_font(240, 120, "PRESS SPACE", Font.default, :color=>[200, 0, 0])
+      if Input.key_push?(K_SPACE)
+        player = Player.new
+        items = Items.new
+        GAME_INFO[:score] = 0
+        GAME_INFO[:scene] = :playing
+      end
+    end
   end
 end
