795850bbaed2559a7a982b170ad1ff745ce8c603
bonochof <ryos427@yahoo.co.jp>
2018-10-14 14:46:38 +0900

    move character

--- main.rb
+++ main.rb
@@ -5,10 +5,18 @@ GROUND_Y = 400
 Image.register(:player, 'images/player.png')
 
 Window.load_resources do
+  x = Window.width / 2
+  
   Window.loop do
+    if Input.key_down?(K_LEFT)
+      x -= 8
+    elsif Input.key_down?(K_RIGHT)
+      x += 8
+    end
+    
     Window.draw_box_fill(0, 0,Window.width, GROUND_Y, [128, 255, 255])
     Window.draw_box_fill(0, GROUND_Y, Window.width, Window.height, [0, 128, 0])
     
-    Window.draw(240, GROUND_Y - Image[:player].height, Image[:player])
+    Window.draw(x, GROUND_Y - Image[:player].height, Image[:player])
   end
 end
